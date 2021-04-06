d0e7f15e05576cd97b5c5a1e898a3be3132eb721
Ollie <myself.ollie.lee@gmail.com>
2021-04-06 21:32:35 +0800

    set up route

--- OnlineAssessment/__init__.py
+++ OnlineAssessment/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+from OnlineAssessment.core.views import core
+
+# Register the apps
+app.register_blueprint(core)
--- OnlineAssessment/core/views.py
+++ OnlineAssessment/core/views.py
@@ -1,4 +1,18 @@
 from flask import render_template,request,Blueprint
-from puppycompanyblog.models import BlogPost
 
+#using blueprint to help us do routing
 core = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    '''
+    This is the home page view.
+    '''
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    '''
+    one promoting the theme and purpose to users;
+    '''
+    return render_template('info.html')
