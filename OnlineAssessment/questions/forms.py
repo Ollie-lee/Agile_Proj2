3f12e9e9d7cdae48c2ba1008e1fa63d9ac0f3269
jialing <test@gmail.com>
2021-05-11 05:54:14 +0800

    create deleteQuestionForm

--- OnlineAssessment/questions/forms.py
+++ OnlineAssessment/questions/forms.py
@@ -3,9 +3,12 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
-class QuestionForm(FlaskForm):
+class CreateQuestionForm(FlaskForm):
     # grab the date automatically from the Model later
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     correct_answer = TextAreaField('Correct Answer', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class DeleteQuestionForm(FlaskForm):
+    submit = SubmitField('Delete')
