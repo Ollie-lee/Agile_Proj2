aa3ce60718ce1889dcadefc64ef909b22d81a205
jialing <test@gmail.com>
2021-05-16 22:56:25 +0800

    modified test view function

--- OnlineAssessment/tests/views.py
+++ OnlineAssessment/tests/views.py
@@ -2,8 +2,7 @@ from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import current_user, login_required
 from OnlineAssessment import db
 from OnlineAssessment.models import Test, Question, Answer
-from sqlalchemy.sql.expression import func, select
-from OnlineAssessment.tests.forms import AnswerForm, TestForm
+from OnlineAssessment.tests.forms import AnswerForm
 
 tests = Blueprint('tests', __name__)
 
@@ -30,10 +29,7 @@ def test(test_id, question_num):
     questions = test.questions
     answers = Answer.query.filter_by(test_id=test_id).all()
     form = AnswerForm()
-    testForm = TestForm()
     if form.validate_on_submit():
-        # get the current question id
-        question_id=form.question.data,
         answer = Answer(
             # get the value of the option selected by the user
             content=form.content.data,
@@ -73,30 +69,3 @@ def my_results():
         tests = []
     return render_template('my_results.html', tests=tests)
 
-
-'''
-@tests.route('/test/<int:test_id>', methods=['GET', 'POST'])
-@login_required
-def test(test_id):
-    test = Test.query.get_or_404(test_id)
-    answers = Answer.query.filter_by(test_id=test_id).all()
-    form = AnswerForm()
-    testForm = TestForm()
-    if form.validate_on_submit():
-        answer = Answer(
-            content=form.content.data,
-            user_id=current_user.id,
-            question_id=form.question.data,
-            test_id=test_id,
-        )
-        db.session.add(answer)
-        db.session.commit()
-        flash("Answer uploaded!")
-        return redirect('test/{}'.format(test_id))
-    if testForm.validate_on_submit():
-        test.finish = True
-        db.session.commit()
-        flash("Test submitted!")
-        return redirect(url_for('core.index'))
-    return render_template('test.html', test=test, form=form, answers=answers)
-'''
